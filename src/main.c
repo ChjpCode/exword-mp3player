b32e22c11bba903b308bb3a85680d6ef0b618c7e
ChjpCode <126651158+ChjpCode@users.noreply.github.com>
2026-07-10 08:54:18 +0700

    Add files via upload

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 #include <sh4a/input/keypad.h>
 #include <graphics/text.h>
 
-void *memmove(void *dest, const void *src, size_t n) {
+void *memmove(void *dest, const void *src, unsigned int n) {
     unsigned char *d = dest;
     const unsigned char *s = src;
     if (d < s) {
@@ -17,7 +17,7 @@ void *memmove(void *dest, const void *src, size_t n) {
     }
     return dest;
 }
-int strncmp(const char *s1, const char *s2, size_t n) {
+int strncmp(const char *s1, const char *s2, unsigned int n) {
     while (n && *s1 && (*s1 == *s2)) { ++s1; ++s2; --n; }
     if (n == 0) return 0;
     return (*(unsigned char *)s1 - *(unsigned char *)s2);
